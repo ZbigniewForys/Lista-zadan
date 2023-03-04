92affe0434d7a673824b55494d0ea31269f6d093
Zbigniew Forys <zbigniew.forys@wp.pl>
2023-03-05 00:58:58 +0100

    og:image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 {
     const tasks = [];
+
     const changeDone = (indexDone) => {
         tasks[indexDone].done = !tasks[indexDone].done;
         render();
@@ -19,54 +20,55 @@
         render()
     }
 
-    const defineClickButtonAddTask = () => {
+    const AddTaskButtonClick = () => {
         const newTaskContent = document.querySelector(".js-newTask");
         const buttonAddTask = document.querySelector(".js-buttonAddTask");
         buttonAddTask.addEventListener("click", () => {
-            if (newTaskContent.value.trim() !== "") {
-                addTask(newTaskContent.value);
-                newTaskContent.value = "";
-            }
+            addTask(newTaskContent.value.trim());
+            newTaskContent.value = "";
             newTaskContent.focus();
         })
     }
 
-    const render = () => {
-        let htmlText = "";
-        for (const task of tasks) {
-            htmlText +=
-                ` <li class="tasksList__line" > `;
-            htmlText +=
-                ` <button class="tasksList__button js-button" > `
-            htmlText += `                                         
-                   ${task.done ? "✓" : ""}`;
-            htmlText += `                                                       
-           </button> 
-                <p class="tasksList__paragraph" 
-                ${task.done ? "style = \"color:rgb(128,128,128) ;  text-decoration:line-through\"" : ""}>
-                ${task.content}</p>
-                <button class="tasksList__button tasksList__button--remove js-buttonRemove">🗑</button> 
-                     </li > `;
-        };
-
-        document.querySelector(".js-tasksList").innerHTML = htmlText;
-
+    const buttonRemoveTask = () => {
         const removeButtons = document.querySelectorAll(".js-buttonRemove");
-
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
             });
         });
+    }
 
+    const buttonChangeDone = () => {
         const changeDoneButtons = document.querySelectorAll(".js-button");
         changeDoneButtons.forEach((changeDoneButton, index) => {
             changeDoneButton.addEventListener("click", () => {
                 changeDone(index);
-
-
             });
         });
+    }
+
+    const renderTasks = () => {
+        let htmlText = "";
+        for (const task of tasks) {
+            htmlText +=
+                ` <li class="tasksList__line" > 
+                    <button class="tasksList__button js-button" > 
+                       ${task.done ? "✓" : ""}                                             
+                    </button> 
+                    <p class="tasksList__paragraph 
+                       ${task.done ? " tasksList__paragraph--done " : ""}">
+                       ${task.content}</p>
+                     <button class="tasksList__button tasksList__button--remove js-buttonRemove">🗑</button> 
+                 </li > `;
+        };
+        document.querySelector(".js-tasksList").innerHTML = htmlText;
+    }
+
+    const render = () => {
+        renderTasks();
+        buttonRemoveTask();
+        buttonChangeDone();
     }
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -76,7 +78,7 @@
     }
 
     const init = () => {
-        defineClickButtonAddTask();
+        AddTaskButtonClick();
         render();
         const form = document.querySelector(".js-form");
         form.addEventListener("submit", onFormSubmit);
